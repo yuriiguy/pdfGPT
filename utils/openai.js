2edba7a634e0e7d9d5e1cb1db62eeca2425abdaf
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:26:10 +0300

    Update openai.js

--- utils/openai.js
+++ utils/openai.js
@@ -1,10 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 const { apiKey } = require('../config');
-
 const configuration = new Configuration({
   apiKey,
 });
-
 const openai = new OpenAIApi(configuration);
-
 module.exports = openai;
