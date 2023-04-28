8c718ba745511afa60503749ff9f6368a7ff9cf4
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:51:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,22 @@ const ask = require('./utils/ask');
 
 async function loadingPdf(pdfPath) {
   const pdfName = getPdfName(pdfPath);
-  // 构建内容树
+  // побудувати дерево вмісту
   const docTree = await buildDocTreeFromPdf(pdfPath);
   // const fs = require('fs');
   // fs.writeFileSync('./temp.json', JSON.stringify(docTree))
-  // 构建知识库
+  // створити базу знань
   const knowledge = await buildKnowledgeFromDocTree(docTree, pdfName);
-  // // 构建知识库向量
+  // // Створіть вектори бази знань
   await buildKnowledgeEmbeddings(knowledge, pdfName);
 }
 
 async function askQuestion(question, pdfName) {
-  console.log(`AI 正在努力回答您的问题『${question}』，请稍作等待...\n`);
+  console.log(`AI Намагаюся відповісти на ваше запитання『${question}』，будь ласка, зачекайте...\n`);
   const answer = await ask(question, pdfName);
-  console.log(`您的问题『${question}』回答如下：\n==========\n${answer}\n==========\n`);
+  console.log(`твоє запитання『${question}"Дайте відповідь на наступне：\n==========\n${answer}\n==========\n`);
   return answer;
 }
-
 module.exports = {
   loadingPdf,
   askQuestion,
