26778374c63b0343914d614e4ede62b33b0689c6
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:35:56 +0300

    Update pdf.js

--- utils/pdf.js
+++ utils/pdf.js
@@ -5,50 +5,39 @@ const {
   shortenDiseaseIntro,
   shortenContent,
 } = require('./content');
-
-// 封面
+// обкладинка
 const PAGE_TYPE_COVER = 0;
-// 目录
+// Зміст
 const PAGE_TYPE_CATALOG = 1;
-// 正文
+// текст
 const PAGE_TYPE_MAIN = 2;
-
 const TITLE_SPLIT = '__TITLE__';
 const QUOTE_SPLIT = '__QUOTE__';
 const REF_SPLIT = '__REF__';
-
 function buildDocTree(longStr) {
-  const [, ...sections] = longStr.split(TITLE_SPLIT); // 将字符串划分成 section 数组
-
+  const [, ...sections] = longStr.split(TITLE_SPLIT); // розділіть рядок на section масиви
   const treeNodes = sections
     .map(section => {
       let [titleNo, ...content] = section.split(' ');
       if (titleNo.endsWith('.')) {
         titleNo = titleNo.slice(0, -1);
       }
-
       const matchedTitleNo = titleNo.match(/^\d+(\.\d*)*\.?/)?.[0];
-
       let joinedContent = content.join(' ');
-
-      // 说明标题中有非纯数字标题的内容，把这部分内容拼接到正文中
+      // Поясніть, що в заголовку є вміст, який не є чисто цифровим заголовком, і поєднайте цю частину вмісту в текст
       if (matchedTitleNo !== titleNo) {
         const titleContent = titleNo.replace(/^\d+(\.\d*)*\.?/, '');
         joinedContent = titleContent + ' ' + joinedContent;
       }
-
       let tokenLength = encode(joinedContent).length;
-
-      // 疾病介绍内容特别长，可以阉割掉具体疾病的详细信息
+      // Зміст введення хвороби дуже довгий, і детальну інформацію про конкретну хворобу можна каструвати
       if (isDiseaseIntro(tokenLength, joinedContent)) {
         joinedContent = shortenDiseaseIntro(joinedContent);
       } else if (tokenLength > 4000) {
-        // 不是疾病介绍也特别长的，采用字典压缩法压缩
+        // Справа не в тому, що введення хвороби також дуже довге, і воно стискається за допомогою методу стиснення словника
         joinedContent = shortenContent(joinedContent);
       }
-
       tokenLength = encode(joinedContent).length;
-
       return {
         titleNo: matchedTitleNo || titleNo,
         content: joinedContent,
@@ -59,7 +48,6 @@ function buildDocTree(longStr) {
     })
     // .map(node => {
     //   const { content } = node;
-
     //   if (content.indexOf(QUOTE_SPLIT)) {
     //     const regex = /__QUOTE__([0-9.]+)/g;
     //     let match;
@@ -77,7 +65,7 @@ function buildDocTree(longStr) {
 
 function isCatalogPage({ items }) {
   const pageContent = items.map(i => i.str).join('');
-  if (pageContent.indexOf('条款目录') > -1) {
+  if (pageContent.indexOf('Каталог термінів') > -1) {
     return true;
   }
   if (pageContent.split(/(?=\d+.\d+)/).length > 10) {
@@ -85,24 +73,20 @@ function isCatalogPage({ items }) {
   }
 }
 
-// 将注释内容拼接到正文中
+// Вставте вміст анотації в основний текст
 function moveNoteToMain(items) {
   const { mainFontHeight, titlePositionX, pageNumberPositionY } =
     getPageMetaData(items);
-
   const isRefTitle = item =>
     Math.abs(item.transform[4] - titlePositionX) < 2 &&
     item.height / mainFontHeight < 0.7;
-
   const refSplitIndex = items.findIndex(isRefTitle);
-
   if (refSplitIndex < 0) {
     return items;
   }
-
-  // 正文
+  // текст
   const mainItems = items.slice(0, refSplitIndex);
-  // 注释
+  // Примітка
   items
     .slice(refSplitIndex)
     .map(refItem => {
@@ -117,7 +101,6 @@ function moveNoteToMain(items) {
       const [refNo, ...content] = refContent.split(' ');
       if (refNo && content.length) {
         const mainItem = mainItems.find(i => i.str.trim() === refNo);
-
         if (!mainItem) {
           return;
         }
@@ -133,7 +116,6 @@ async function getPdfItems(pdfPath) {
   await pdfjs.getDocument(pdfPath).promise.then(doc => {
     const numPages = doc.numPages;
     let lastPromise = doc.getMetadata();
-
     const loadPage = function (pageNum) {
       return doc.getPage(pageNum).then(page => {
         return page
@@ -142,15 +124,15 @@ async function getPdfItems(pdfPath) {
             // includeMarkedContent: true,
           })
           .then(pageData => {
-            // 如果之前是封面，当前页已经是目录页了，状态改为目录页
+            // Якщо раніше це була обкладинка, поточна сторінка вже є сторінкою каталогу, а статус змінено на сторінку каталогу
             if (pageType === PAGE_TYPE_COVER && isCatalogPage(pageData)) {
               pageType = PAGE_TYPE_CATALOG;
             }
-            // 如果之前是目录页，当前页已经不是目录页，状态改为正文页
+            // Якщо раніше це була сторінка каталогу, поточна сторінка більше не є сторінкою каталогу, а статус змінюється на текстову сторінку
             if (pageType === PAGE_TYPE_CATALOG && !isCatalogPage(pageData)) {
               pageType = PAGE_TYPE_MAIN;
             }
-            // 从正文开始，push内容
+            // Почніть із основної частини, натисніть вміст
             if (pageType === PAGE_TYPE_MAIN) {
               const contentItems = pageData.items.map(i => ({ ...i, pageNum }));
               pdfItems.push(...moveNoteToMain(contentItems));
@@ -159,8 +141,7 @@ async function getPdfItems(pdfPath) {
           });
       });
     };
-    // Loading of the first page will wait on metadata and subsequent loadings
-    // will wait on the previous pages.
+    // Завантаження першої сторінки очікуватиме на метаданих, а наступні завантаження – на попередніх сторінках.
     for (let i = 1; i <= numPages; i++) {
       lastPromise = lastPromise.then(() => loadPage(i));
     }
@@ -168,21 +149,17 @@ async function getPdfItems(pdfPath) {
   });
   return pdfItems;
 }
-
 const isTitleNo = (items, itemIndex) => {
   const item = items[itemIndex];
   const nextItem = items[itemIndex + 1];
-
   const { str: itemContent } = item;
-  // 一般来说，太长字符的肯定不是标题，减少后续的正则校验开销
+  // Взагалі кажучи, надто довгі символи точно не є заголовками, що зменшує витрати на подальшу регулярну перевірку
   if (itemContent.length > 20) {
     return false;
   }
-
   if (nextItem && nextItem.str.trim() === '页') {
     return item;
   }
-
   return /^\d+(\.\d*)*\.?/.test(itemContent.trim());
   // return /^\d+(\.\d*)*\.?$/.test(itemContent.trim());
 };
@@ -191,7 +168,6 @@ function getPageMetaData(items) {
   const fontHeightCountMap = {};
   const numberPositionXCountMap = {};
   let minPositionY = Infinity;
-
   items.forEach((cur, index) => {
     const { height, transform } = cur;
     const positionX = transform[4];
@@ -200,17 +176,13 @@ function getPageMetaData(items) {
       console.log(cur);
     }
     const isTitle = isTitleNo(items, index);
-
     fontHeightCountMap[height] = (fontHeightCountMap[height] || 0) + 1;
-
     minPositionY = Math.min(minPositionY, positionY);
-
     if (isTitle) {
       numberPositionXCountMap[positionX] =
         (numberPositionXCountMap[positionX] || 0) + 1;
     }
   }, {});
-
   const sortedHeights = Object.keys(fontHeightCountMap)
     .map(height => {
       return {
@@ -219,7 +191,6 @@ function getPageMetaData(items) {
       };
     })
     .sort((a, b) => b.counts - a.counts);
-
   const sortedPositionXs = Object.keys(numberPositionXCountMap)
     .map(positionX => {
       return {
@@ -229,14 +200,13 @@ function getPageMetaData(items) {
     })
     .filter(i => i.positionX < 100)
     .sort((a, b) => b.counts - a.counts);
-  // 处于较左端的，否则会被有些列表项的数字污染
-
+  // у крайньому лівому куті, інакше він буде забруднений кількістю деяких елементів списку
   return {
-    // 使用最多的字体大小，有理由相信，它就是正文字体大小
+    // Найбільш використовуваним розміром шрифту, і це розумно вважати, є розмір основного шрифту
     mainFontHeight: sortedHeights[0].height,
-    // 即是数字，又是持续在一个x坐标体现的，有理由相信，它就是标题数字
+    // Це число, і воно безперервно відображається в координаті х.Є підстави вважати, що це номер назви
     titlePositionX: sortedPositionXs?.[0]?.positionX,
-    // 最靠底的部分，有理想相信，它是页码的位置。但要小于60，否则是无页码的PDF
+    // Нижня частина, в ідеалі, це розташування номера сторінки. Але він має бути меншим за 60, інакше це PDF без номерів сторінок
     pageNumberPositionY: minPositionY < 60 ? minPositionY : undefined,
   };
 }
@@ -251,31 +221,25 @@ function rebuildPdfItems(items) {
       const prevItem = items[index - 1];
       const positionX = transform[4];
       const positionY = transform[5];
-
-      // 页码数据不需要
+      // Дані про номер сторінки не потрібні
       if (pageNumberPositionY === positionY) {
         return null;
       }
-
-      if (itemContent.startsWith('附表')) {
+      if (itemContent.startsWith('графік')) {
         item.str = `${TITLE_SPLIT}${itemContent.trim()}`;
         return item;
       }
-
       if (!isTitleNo(items, index)) {
         return item;
       }
-
-      // 大标题，允许一定误差
+      // Великий заголовок, допустіть помилку
       if (Math.abs(positionX - titlePositionX) < 2) {
         item.str = `${TITLE_SPLIT}${itemContent.trim()}`;
         return item;
       }
-
       // const prevHeight = prevItem?.height;
       // const nextHeight = nextItem.height;
-
-      // 引用注释
+      // цитати примітки
       // if (
       //   prevItem &&
       //   currentHeight < prevHeight &&
@@ -284,7 +248,6 @@ function rebuildPdfItems(items) {
       //   item.str = `${QUOTE_SPLIT}${itemContent}`;
       //   return item;
       // }
-
       return item;
     })
     .filter(Boolean);
@@ -298,7 +261,6 @@ async function buildDocTreeFromPdf(pdfPath) {
   // fs.writeFileSync('./tempItems.json', JSON.stringify(itemsWithTreeInfo));
   return buildDocTree(itemsWithTreeInfo.map(i => i.str).join(''));
 }
-
 module.exports = {
   buildDocTreeFromPdf,
   getPdfItems,
