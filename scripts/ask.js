757a790f13a2f36abd8f0e8ab4415f6df8c08a28
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:11:26 +0300

    Update ask.js

--- scripts/ask.js
+++ scripts/ask.js
@@ -1,8 +1,6 @@
 const config = require('../config');
 const { askQuestion } = require('../index');
-
 const { pdfName, questions } = config;
-
 (async () => {
   for (let index = 0; index < questions.length; index++) {
     const question = questions[index];
