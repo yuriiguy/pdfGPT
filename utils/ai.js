ab3f10a458a2674615614d63c8fbd1d5d371af4d
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:19:08 +0300

    Update ai.js

--- utils/ai.js
+++ utils/ai.js
@@ -1,9 +1,7 @@
 const crypto = require('crypto');
 const { encode } = require('gpt-3-encoder');
-
 const openai = require('./openai');
 const cache = require('../cache');
-
 function buildHash(content) {
   return crypto.createHash('md5').update(content).digest('hex');
 }
@@ -19,11 +17,10 @@ async function createCompletion({
     max_tokens,
     temperature,
   });
-
   return strip(completion?.data?.choices?.[0].text, ['\n']).trim();
 }
 
-// 去头尾指定字符
+// Видалити вказані символи на початку та в кінці
 const strip = (str, chars) => {
   let newStr = str;
   chars.forEach(char => {
@@ -31,20 +28,17 @@ const strip = (str, chars) => {
   });
   return newStr;
 };
-
 const withCache =
   (wrappedFn, suffix, getContent) => async (arg, cacheFileName) => {
     const content = getContent(arg);
     const cacheName = `${cacheFileName}_${suffix}`;
-    // 文本太长，hash一下
+    // Текст задовгий, будь-ласка, слід відсікти його
     const hash = buildHash(content);
     const cacheValue = cache.get(cacheName, hash);
     if (cacheValue) {
       return cacheValue;
     }
-
     const rs = await wrappedFn(arg);
-
     cache.set(cacheName, hash, rs);
     return rs;
   };
@@ -52,22 +46,20 @@ const withCache =
 async function getSummary({ content, tokenLength }) {
   const promptContext =
     content.indexOf('|上文中a:') >= -1
-      ? `'''{{content}}'''基于字典翻译并返回内容摘要：`
-      : `'''{{content}}'''基于命名实体识别构建内容摘要：`;
+      ? `'''{{content}}'''Перекладайте на основі словника та повертайте короткий виклад вмісту：`
+      : `'''{{content}}'''Побудова резюмації вмісту на основі розпізнавання іменованих сутностей：`;
   const contentTokenLength = tokenLength || encode(content).length;
   const promptContextTokenLength = encode(promptContext).length;
-
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: promptContext.replace('{{content}}', content),
-    // 1000 ~ 4096，最大也不能超过1000
+    // 1000 ~ 4096，Максимум не може перевищувати 1000
     max_tokens: Math.min(
       4096 - contentTokenLength - promptContextTokenLength,
       1000,
     ),
     temperature: 0,
   });
-
   return strip(completion?.data?.choices?.[0].text, ['\n']);
 }
 
@@ -77,28 +69,24 @@ async function createEmbedding(input) {
       model: 'text-embedding-ada-002',
       input: input,
     }),
-    // 向量化很快，休息一下，防止调用超限(默认最多每分钟60次)
+    // Векторизація швидка, зробіть перерву, щоб запобігти переповненню виклику (за замовчуванням до 60 разів на хвилину)
     await sleep(3000),
   ]);
-
   return response.data.data[0].embedding;
 }
 
 async function askInsQuestion({ question, knowledge }) {
   const prompt = `
-    以下是某保险产品条款的部分
+    Нижче наведено частину умов страхового продукту
     '''${knowledge}'''
-    请基于对保险的理解与该部分条款内容，回答如下问题：
+    Будь ласка, дайте відповіді на наступні питання, виходячи з вашого розуміння страхування та змісту цієї частини умов：
     ${question}。
-    答案：
+    Відповідь：
     `;
-
   const promptTokenLength = encode(prompt).length;
-
   return createCompletion({ prompt, max_tokens: 4096 - promptTokenLength });
 }
-
-// 防止超过每分钟调用限制
+// Запобігайте перевищенню ліміту дзвінків за хвилину
 const sleep = time =>
   new Promise(resolve => {
     setTimeout(resolve, time);
