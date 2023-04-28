27c4cb9c5fa10a034da0abbdc9f88d1d22dd51fc
Dubovenko Yurii <94867282+yuriiguy@users.noreply.github.com>
2023-04-28 18:12:06 +0300

    Update load.js

--- scripts/load.js
+++ scripts/load.js
@@ -1,9 +1,6 @@
 const config = require('../config');
 const { loadingPdf } = require('../index');
 const { getPdfPath } = require('../utils/fs');
-
 const { pdfName } = config;
-
 const pdfPath = getPdfPath(pdfName);
-
 loadingPdf(pdfPath);
